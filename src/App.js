62c9e0a5adc50a15931967e300a71bd39364a4bd
StudyFYCorp <studyfycorp@gmail.com>
2024-11-19 14:48:34 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ import Login from './pages/Login';
 import GrupoMentoria from './pages/GrupoMentoria';
 import ChatIA from './pages/chatIA'
 import ChatPrivado from './pages/ChatPrivado'
-import Atividades from './pages/Atividades';
+import Atividades from './pages/Questoes';
 import Ajuda from './pages/Ajuda';
 import CadernoVirtual from './pages/CadernoVirtual';
 import Notificacao from './pages/Notificacao';
 import Rank from './pages/Rank';
-import Perfil  from './pages/PerfilAlunoMentor';
-import PerfilAluno from './pages/PerfilAluno';
+import Perfil from './pages/perfil';
 import EsqueceuSenha from './pages/EsqueceuSenha';
 import MudarSenha from './pages/MudarSenha'
 import Emblemas from './pages/Emblemas';
+import VisualizacaoMentorias from './pages/visualizacaoMentoria';
+import CriarGrupoMentoria from './pages/CriarGrupoMentoria';
+import GrupoMentoriaPrevia from './pages/GrupoMentoriaPrevia';
+import PerfilConfiguracao from './pages/perfilConfiguracao';
+import TelaAtividades from './pages/telaAtividades';
+import Subiurank from './pages/SubiuRank';
+import ManteveRank from './pages/ManteveRank';
+import DesceuRank from './pages/DesceuRank';
+import MontagemAtividades from './pages/MontagemAtividades';
+import MontagemAtividadesOrganizar from './pages/MontagemAtividadeOrganizar';
+import MontagemAtividadesTexto from './pages/MontagemAtividadeTexto';
 
 import * as C from './styles/app';
 
@@ -25,10 +35,19 @@ function App() {
     <C.Container>
       <Router>
         <Routes>
-          <Route exact path='/' element= {<PerfilAluno/>} />
-          <Route path='/PerfilAluno' element={<Perfil/>} />
-          <Route exact path='/' element= {<GrupoMentoria/>} />
-          <Route path='/Perfil' element={<Perfil/>}></Route>
+          <Route exact path='/' element= {<MontagemAtividades/>} />
+          <Route path='/montagem-atividades' element= {<MontagemAtividades/>} />
+          <Route path='/montagem-atividades-organizar' element= {<MontagemAtividadesOrganizar/>} />
+          <Route path='/montagem-atividades-texto' element= {<MontagemAtividadesTexto/>} />
+          <Route path = '/subiu-rank' element={<Subiurank/>}/>
+          <Route path = '/desceu-rank' element={<DesceuRank/>}/>
+          <Route path = '/manteve-rank' element={<ManteveRank/>}/>
+          <Route path = '/perfil-configuracao' element={<PerfilConfiguracao/>}/>
+          <Route path = '/tela-atividades' element={<TelaAtividades/>}/>
+          <Route exact path ='/criar-grupo-mentoria' element = {<CriarGrupoMentoria/>}/>
+          <Route exact path ='/visualizar-mentorias' element = {<VisualizacaoMentorias/>}/>
+          <Route exact path ='/grupo-mentoria-previa/:id' element = {<GrupoMentoriaPrevia/>}/>
+          <Route path='/perfil' element={<Perfil/>}></Route>
           <Route path='/atividades' element= {<Atividades/>} />
           <Route path='/caderno-virtual' element= {<CadernoVirtual/>} />
           <Route path='/rank' element= {<Rank/>} />
@@ -38,7 +57,7 @@ function App() {
           <Route path='/chatIA' element= {<ChatIA/>} />
           <Route path='/login' element= {<Login/>} />
           <Route path='/cadastro' element={<Cadastro />}/>
-          <Route path='/grupo-mentoria' element={<GrupoMentoria />}/>
+          <Route path='/grupo-mentoria/:id' element={<GrupoMentoria />}/>
           <Route path='/esqueceu-senha' element={<EsqueceuSenha />}/>
           <Route path='/mudar-senha' element={<MudarSenha />}/>
           <Route path='/emblemas' element={<Emblemas />}/>
