a692c9472cc442728b3557c321ec395cd07ba34a
StudyFYCorp <studyfycorp@gmail.com>
2024-11-19 14:48:04 -0300

    Add files via upload

---
+++ src/styles/botoesconfiguracao.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const BotoesConfiguracao = styled.div`
+   display: flex;
+   background-color: #fee101;
+   align-items: center;
+   justify-content: center;
+   font-size: 1.2vw;
+   font-weight: bold;
+   min-height: 10%;
+   width: 100%;
+   border: solid 1px #E9CE03;
+   border-radius: 12px;
+   box-shadow: 0 0.5vh 0 0 #E9CE03;
+`
+
+export default BotoesConfiguracao
--- src/styles/tituloTela.js
+++ src/styles/tituloTela.js
@@ -2,7 +2,15 @@ import styled from "styled-components";
 
 export const tituloTela = styled.div`
    font-size: 4vw;
+   margin-top: 1vh;
    margin-bottom: 8vh;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+
+   @media (min-width: 768px){
+      font-size: 2vw;
+   }
 `
 
 export default tituloTela
